4a4ab7790cf563d08f9956f3d2b710817016063e
jeff-rareearth <jeff@rareearth.technology>
2025-11-21 11:42:04 -0600

    Added Events Api

--- src/FamMan.AppHost/AppHost.cs
+++ src/FamMan.AppHost/AppHost.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.FamMan>("famman");
 
+builder.AddProject<Projects.FamMan_Api_Events>("famman-api-events");
+
 builder.Build().Run();
