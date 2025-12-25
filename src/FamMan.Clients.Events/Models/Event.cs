68ecbf30342eec3f947fe7a2be43a685f192dc7b
Josh Fuqua <103945778+JoshDFuqua@users.noreply.github.com>
2025-12-25 08:36:06 -0500

    Update events client to use the correct namespace (#10)
    * Update events client to use the correct namespace
    
    * Add RootNamespace to events client

--- src/FamMan.Clients.Events/Models/Event.cs
+++ src/FamMan.Clients.Events/Models/Event.cs
@@ -5,79 +5,79 @@ using Microsoft.Kiota.Abstractions.Serialization;
 using System.Collections.Generic;
 using System.IO;
 using System;
-namespace Events.Client.Models
+namespace FamMan.Clients.Models
 {
-    [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
-    #pragma warning disable CS1591
-    public partial class Event : IAdditionalDataHolder, IParsable
-    #pragma warning restore CS1591
-    {
-        /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
-        public IDictionary<string, object> AdditionalData { get; set; }
-        /// <summary>The CreatedAt property</summary>
-        public DateTimeOffset? CreatedAt { get; set; }
-        /// <summary>The Description property</summary>
+  [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
+#pragma warning disable CS1591
+  public partial class Event : IAdditionalDataHolder, IParsable
+#pragma warning restore CS1591
+  {
+    /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
+    public IDictionary<string, object> AdditionalData { get; set; }
+    /// <summary>The CreatedAt property</summary>
+    public DateTimeOffset? CreatedAt { get; set; }
+    /// <summary>The Description property</summary>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
-        public string? Description { get; set; }
+    public string? Description { get; set; }
 #nullable restore
 #else
         public string Description { get; set; }
 #endif
-        /// <summary>The EventType property</summary>
+    /// <summary>The EventType property</summary>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
-        public string? EventType { get; set; }
+    public string? EventType { get; set; }
 #nullable restore
 #else
         public string EventType { get; set; }
 #endif
-        /// <summary>The Id property</summary>
-        public Guid? Id { get; set; }
-        /// <summary>The IsActive property</summary>
-        public bool? IsActive { get; set; }
-        /// <summary>The Name property</summary>
+    /// <summary>The Id property</summary>
+    public Guid? Id { get; set; }
+    /// <summary>The IsActive property</summary>
+    public bool? IsActive { get; set; }
+    /// <summary>The Name property</summary>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
-        public string? Name { get; set; }
+    public string? Name { get; set; }
 #nullable restore
 #else
         public string Name { get; set; }
 #endif
-        /// <summary>The RecurrenceRules property</summary>
+    /// <summary>The RecurrenceRules property</summary>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
-        public global::Events.Client.Models.Event_RecurrenceRules? RecurrenceRules { get; set; }
+    public global::FamMan.Clients.Models.Event_RecurrenceRules? RecurrenceRules { get; set; }
 #nullable restore
 #else
-        public global::Events.Client.Models.Event_RecurrenceRules RecurrenceRules { get; set; }
+        public global::FamMan.Clients.Models.Event_RecurrenceRules RecurrenceRules { get; set; }
 #endif
-        /// <summary>The UpdatedAt property</summary>
-        public DateTimeOffset? UpdatedAt { get; set; }
-        /// <summary>
-        /// Instantiates a new <see cref="global::Events.Client.Models.Event"/> and sets the default values.
-        /// </summary>
-        public Event()
-        {
-            AdditionalData = new Dictionary<string, object>();
-        }
-        /// <summary>
-        /// Creates a new instance of the appropriate class based on discriminator value
-        /// </summary>
-        /// <returns>A <see cref="global::Events.Client.Models.Event"/></returns>
-        /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-        public static global::Events.Client.Models.Event CreateFromDiscriminatorValue(IParseNode parseNode)
-        {
-            if(ReferenceEquals(parseNode, null)) throw new ArgumentNullException(nameof(parseNode));
-            return new global::Events.Client.Models.Event();
-        }
-        /// <summary>
-        /// The deserialization information for the current model
-        /// </summary>
-        /// <returns>A IDictionary&lt;string, Action&lt;IParseNode&gt;&gt;</returns>
-        public virtual IDictionary<string, Action<IParseNode>> GetFieldDeserializers()
-        {
-            return new Dictionary<string, Action<IParseNode>>
+    /// <summary>The UpdatedAt property</summary>
+    public DateTimeOffset? UpdatedAt { get; set; }
+    /// <summary>
+    /// Instantiates a new <see cref="global::FamMan.Clients.Models.Event"/> and sets the default values.
+    /// </summary>
+    public Event()
+    {
+      AdditionalData = new Dictionary<string, object>();
+    }
+    /// <summary>
+    /// Creates a new instance of the appropriate class based on discriminator value
+    /// </summary>
+    /// <returns>A <see cref="global::FamMan.Clients.Models.Event"/></returns>
+    /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
+    public static global::FamMan.Clients.Models.Event CreateFromDiscriminatorValue(IParseNode parseNode)
+    {
+      if (ReferenceEquals(parseNode, null)) throw new ArgumentNullException(nameof(parseNode));
+      return new global::FamMan.Clients.Models.Event();
+    }
+    /// <summary>
+    /// The deserialization information for the current model
+    /// </summary>
+    /// <returns>A IDictionary&lt;string, Action&lt;IParseNode&gt;&gt;</returns>
+    public virtual IDictionary<string, Action<IParseNode>> GetFieldDeserializers()
+    {
+      return new Dictionary<string, Action<IParseNode>>
             {
                 { "CreatedAt", n => { CreatedAt = n.GetDateTimeOffsetValue(); } },
                 { "Description", n => { Description = n.GetStringValue(); } },
@@ -85,27 +85,27 @@ namespace Events.Client.Models
                 { "Id", n => { Id = n.GetGuidValue(); } },
                 { "IsActive", n => { IsActive = n.GetBoolValue(); } },
                 { "Name", n => { Name = n.GetStringValue(); } },
-                { "RecurrenceRules", n => { RecurrenceRules = n.GetObjectValue<global::Events.Client.Models.Event_RecurrenceRules>(global::Events.Client.Models.Event_RecurrenceRules.CreateFromDiscriminatorValue); } },
+                { "RecurrenceRules", n => { RecurrenceRules = n.GetObjectValue<global::FamMan.Clients.Models.Event_RecurrenceRules>(global::FamMan.Clients.Models.Event_RecurrenceRules.CreateFromDiscriminatorValue); } },
                 { "UpdatedAt", n => { UpdatedAt = n.GetDateTimeOffsetValue(); } },
             };
-        }
-        /// <summary>
-        /// Serializes information the current object
-        /// </summary>
-        /// <param name="writer">Serialization writer to use to serialize this model</param>
-        public virtual void Serialize(ISerializationWriter writer)
-        {
-            if(ReferenceEquals(writer, null)) throw new ArgumentNullException(nameof(writer));
-            writer.WriteDateTimeOffsetValue("CreatedAt", CreatedAt);
-            writer.WriteStringValue("Description", Description);
-            writer.WriteStringValue("EventType", EventType);
-            writer.WriteGuidValue("Id", Id);
-            writer.WriteBoolValue("IsActive", IsActive);
-            writer.WriteStringValue("Name", Name);
-            writer.WriteObjectValue<global::Events.Client.Models.Event_RecurrenceRules>("RecurrenceRules", RecurrenceRules);
-            writer.WriteDateTimeOffsetValue("UpdatedAt", UpdatedAt);
-            writer.WriteAdditionalData(AdditionalData);
-        }
     }
+    /// <summary>
+    /// Serializes information the current object
+    /// </summary>
+    /// <param name="writer">Serialization writer to use to serialize this model</param>
+    public virtual void Serialize(ISerializationWriter writer)
+    {
+      if (ReferenceEquals(writer, null)) throw new ArgumentNullException(nameof(writer));
+      writer.WriteDateTimeOffsetValue("CreatedAt", CreatedAt);
+      writer.WriteStringValue("Description", Description);
+      writer.WriteStringValue("EventType", EventType);
+      writer.WriteGuidValue("Id", Id);
+      writer.WriteBoolValue("IsActive", IsActive);
+      writer.WriteStringValue("Name", Name);
+      writer.WriteObjectValue<global::FamMan.Clients.Models.Event_RecurrenceRules>("RecurrenceRules", RecurrenceRules);
+      writer.WriteDateTimeOffsetValue("UpdatedAt", UpdatedAt);
+      writer.WriteAdditionalData(AdditionalData);
+    }
+  }
 }
 #pragma warning restore CS0618
