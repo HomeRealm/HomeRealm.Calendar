5d44787bb908c10276c7cecd9ba9e7308ab5359e
Josh Fuqua <103945778+JoshDFuqua@users.noreply.github.com>
2025-12-25 08:36:06 -0500

    Update events client to use the correct namespace (#10)
    * Update events client to use the correct namespace
    
    * Add RootNamespace to events client

--- src/FamMan.Clients.Events/Models/Event_RecurrenceRules.cs
+++ src/FamMan.Clients.Events/Models/Event_RecurrenceRules.cs
@@ -5,51 +5,51 @@ using Microsoft.Kiota.Abstractions.Serialization;
 using System.Collections.Generic;
 using System.IO;
 using System;
-namespace Events.Client.Models
+namespace FamMan.Clients.Models
 {
-    [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
-    #pragma warning disable CS1591
-    public partial class Event_RecurrenceRules : IAdditionalDataHolder, IParsable
-    #pragma warning restore CS1591
+  [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
+#pragma warning disable CS1591
+  public partial class Event_RecurrenceRules : IAdditionalDataHolder, IParsable
+#pragma warning restore CS1591
+  {
+    /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
+    public IDictionary<string, object> AdditionalData { get; set; }
+    /// <summary>
+    /// Instantiates a new <see cref="global::FamMan.Clients.Models.Event_RecurrenceRules"/> and sets the default values.
+    /// </summary>
+    public Event_RecurrenceRules()
     {
-        /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
-        public IDictionary<string, object> AdditionalData { get; set; }
-        /// <summary>
-        /// Instantiates a new <see cref="global::Events.Client.Models.Event_RecurrenceRules"/> and sets the default values.
-        /// </summary>
-        public Event_RecurrenceRules()
-        {
-            AdditionalData = new Dictionary<string, object>();
-        }
-        /// <summary>
-        /// Creates a new instance of the appropriate class based on discriminator value
-        /// </summary>
-        /// <returns>A <see cref="global::Events.Client.Models.Event_RecurrenceRules"/></returns>
-        /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-        public static global::Events.Client.Models.Event_RecurrenceRules CreateFromDiscriminatorValue(IParseNode parseNode)
-        {
-            if(ReferenceEquals(parseNode, null)) throw new ArgumentNullException(nameof(parseNode));
-            return new global::Events.Client.Models.Event_RecurrenceRules();
-        }
-        /// <summary>
-        /// The deserialization information for the current model
-        /// </summary>
-        /// <returns>A IDictionary&lt;string, Action&lt;IParseNode&gt;&gt;</returns>
-        public virtual IDictionary<string, Action<IParseNode>> GetFieldDeserializers()
-        {
-            return new Dictionary<string, Action<IParseNode>>
-            {
-            };
-        }
-        /// <summary>
-        /// Serializes information the current object
-        /// </summary>
-        /// <param name="writer">Serialization writer to use to serialize this model</param>
-        public virtual void Serialize(ISerializationWriter writer)
-        {
-            if(ReferenceEquals(writer, null)) throw new ArgumentNullException(nameof(writer));
-            writer.WriteAdditionalData(AdditionalData);
-        }
+      AdditionalData = new Dictionary<string, object>();
     }
+    /// <summary>
+    /// Creates a new instance of the appropriate class based on discriminator value
+    /// </summary>
+    /// <returns>A <see cref="global::FamMan.Clients.Models.Event_RecurrenceRules"/></returns>
+    /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
+    public static global::FamMan.Clients.Models.Event_RecurrenceRules CreateFromDiscriminatorValue(IParseNode parseNode)
+    {
+      if (ReferenceEquals(parseNode, null)) throw new ArgumentNullException(nameof(parseNode));
+      return new global::FamMan.Clients.Models.Event_RecurrenceRules();
+    }
+    /// <summary>
+    /// The deserialization information for the current model
+    /// </summary>
+    /// <returns>A IDictionary&lt;string, Action&lt;IParseNode&gt;&gt;</returns>
+    public virtual IDictionary<string, Action<IParseNode>> GetFieldDeserializers()
+    {
+      return new Dictionary<string, Action<IParseNode>>
+      {
+      };
+    }
+    /// <summary>
+    /// Serializes information the current object
+    /// </summary>
+    /// <param name="writer">Serialization writer to use to serialize this model</param>
+    public virtual void Serialize(ISerializationWriter writer)
+    {
+      if (ReferenceEquals(writer, null)) throw new ArgumentNullException(nameof(writer));
+      writer.WriteAdditionalData(AdditionalData);
+    }
+  }
 }
 #pragma warning restore CS0618
