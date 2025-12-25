177a3e7e7370c26963465bcbbe16b4d9d2102428
jeff-rareearth <jeff@rareearth.technology>
2025-12-25 10:53:12 -0600

    Updated to mudblazor 9 preview

--- src/FamMan/FamMan/Program.cs
+++ src/FamMan/FamMan/Program.cs
@@ -1,6 +1,10 @@
 using FamMan.Client.Pages;
 using FamMan.Components;
 using MudBlazor.Services;
+using Microsoft.Kiota.Abstractions;
+using Microsoft.Kiota.Abstractions.Authentication;
+using Microsoft.Kiota.Http.HttpClientLibrary;
+using FamMan.Clients;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration
@@ -14,6 +18,35 @@ builder.Services.AddReverseProxy()
 // Add MudBlazor services
 builder.Services.AddMudServices();
 
+// Configure HttpClients with Aspire service discovery for API clients
+builder.Services.AddHttpClient("EventsApiClient", client =>
+{
+  client.BaseAddress = new Uri("https+http://famman-api-events");
+}).AddServiceDiscovery();
+
+builder.Services.AddHttpClient("ChoresApiClient", client =>
+{
+  client.BaseAddress = new Uri("https+http://famman-api-chores");
+}).AddServiceDiscovery();
+
+// Register IRequestAdapter for EventsClient
+builder.Services.AddScoped<IRequestAdapter>(sp =>
+{
+  var authProvider = new AnonymousAuthenticationProvider();
+  var httpClient = sp.GetRequiredService<IHttpClientFactory>().CreateClient("EventsApiClient");
+  return new HttpClientRequestAdapter(authProvider, httpClient: httpClient);
+});
+
+// Register API Clients
+builder.Services.AddScoped<EventsClient>();
+builder.Services.AddScoped<ChoresClient>(sp =>
+{
+  var authProvider = new AnonymousAuthenticationProvider();
+  var httpClient = sp.GetRequiredService<IHttpClientFactory>().CreateClient("ChoresApiClient");
+  var requestAdapter = new HttpClientRequestAdapter(authProvider, httpClient: httpClient);
+  return new ChoresClient(requestAdapter);
+});
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
@@ -24,13 +57,13 @@ app.MapDefaultEndpoints();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseWebAssemblyDebugging();
+  app.UseWebAssemblyDebugging();
 }
 else
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+  app.UseExceptionHandler("/Error", createScopeForErrors: true);
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 }
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
