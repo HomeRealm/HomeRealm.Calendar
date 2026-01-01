d0ee9c97cbe78254f48e41ba10f332342d4ee19a
joshdfuqua <joshdfuqua@gmail.com>
2026-01-01 13:17:11 -0500

    Add template for calendar api

---
+++ src/FamMan.Api.Calendar/Program.cs
@@ -0,0 +1,59 @@
+using FamMan.Api.Calendar;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCalendarServices();
+
+builder.AddServiceDefaults();
+builder.AddNpgsqlDbContext<CalendarDbContext>("famman-calendar");
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+var app = builder.Build();
+
+
+// automatically apply migrations during startup
+// will be moved to background service in future
+using (var scope = app.Services.CreateScope())
+{
+  var dbContext = scope.ServiceProvider.GetRequiredService<CalendarDbContext>();
+  dbContext.Database.Migrate();
+}
+
+app.MapDefaultEndpoints();
+var api = app.MapGroup("/api");
+api.MapCalendarEventsEndpoints();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+  app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+  var forecast = Enumerable.Range(1, 5).Select(index =>
+      new WeatherForecast
+      (
+          DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+          Random.Shared.Next(-20, 55),
+          summaries[Random.Shared.Next(summaries.Length)]
+      ))
+      .ToArray();
+  return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+  public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
