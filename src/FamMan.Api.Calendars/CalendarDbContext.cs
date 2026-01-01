3f8b06b49e197ba3710346e4951518063f4beed1
joshdfuqua <joshdfuqua@gmail.com>
2026-01-01 16:29:41 -0500

    Create CalendarDbContext.cs
    - Includes CalendarEntity table

---
+++ src/FamMan.Api.Calendars/CalendarDbContext.cs
@@ -0,0 +1,16 @@
+﻿using FamMan.Api.Calendars.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FamMan.Api.Calendars;
+
+public class CalendarDbContext(DbContextOptions<CalendarDbContext> options) : DbContext(options)
+{
+  public DbSet<CalendarEntity> Calendars { get; set; } = default!;
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    // automatically apply all configurations from the current assembly
+    // Configuration classes are located in the EntityConfiguration directory. 
+    modelBuilder.ApplyConfigurationsFromAssembly(typeof(CalendarDbContext).Assembly);
+  }
+}
