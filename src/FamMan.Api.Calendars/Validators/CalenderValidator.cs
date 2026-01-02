3ae9018b9238a168aa1757b823e5c991926263d5
joshdfuqua <joshdfuqua@gmail.com>
2026-01-02 12:00:56 -0500

    Update calendar validator
    - Adds ID validation
    - Adds messages to all rules

--- src/FamMan.Api.Calendars/Validators/CalenderValidator.cs
+++ src/FamMan.Api.Calendars/Validators/CalenderValidator.cs
@@ -7,24 +7,28 @@ public class CalendarValidator : AbstractValidator<CalendarEntity>
 {
   public CalendarValidator()
   {
+    RuleFor(x => x.Id)
+      .NotEmpty().WithMessage("An ID is required for this operation")
+      .NotEqual(Guid.Empty).WithMessage("ID cannot be the default GUID");
+
     RuleFor(x => x.Name)
-      .NotEmpty()
-      .MaximumLength(200);
+      .NotEmpty().WithMessage("Name cannot be empty.")
+      .MaximumLength(200).WithMessage("Name must be shorter than 200 characters.");
 
     RuleFor(x => x.Description)
-      .NotEmpty()
-      .MaximumLength(1000);
+      .NotEmpty().WithMessage("Description cannot be empty.")
+      .MaximumLength(1000).WithMessage("Description must be shorter than 1000 characters.");
 
     RuleFor(x => x.Color)
-      .NotEmpty()
-      .MaximumLength(50);
+      .NotEmpty().WithMessage("Color cannot be empty.")
+      .MaximumLength(50).WithMessage("Color must be shorter than 50 characters.");
 
     RuleFor(x => x.Owner)
-      .NotEmpty()
-      .MaximumLength(100);
+      .NotEmpty().WithMessage("Owner cannot be empty.")
+      .MaximumLength(100).WithMessage("Owner must be shorter than 100 characters.");
 
     RuleFor(x => x.Visibility)
-      .NotEmpty()
-      .MaximumLength(10);
+      .NotEmpty().WithMessage("Visibility cannot be empty.")
+      .MaximumLength(10).WithMessage("Visibility must be shorter than 10 characters.");
   }
 }
