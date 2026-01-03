98634fafc6c60fb6313c225401424b6cefb3e348
joshdfuqua <joshdfuqua@gmail.com>
2026-01-03 11:36:58 -0500

    Adds calendar service registration
    - Removes unnecessary weather endpoint placeholder

--- src/FamMan.Api.Calendars/Program.cs
+++ src/FamMan.Api.Calendars/Program.cs
@@ -1,4 +1,6 @@
 using FamMan.Api.Calendars;
+using FamMan.Api.Calendars.Endpoints;
+using FamMan.Api.Calendars.Extensions;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -22,7 +24,7 @@ using (var scope = app.Services.CreateScope())
 
 app.MapDefaultEndpoints();
 var api = app.MapGroup("/api");
-api.MapCalendarEventsEndpoints();
+api.MapCalendarsEndpoints();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -32,28 +34,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-  var forecast = Enumerable.Range(1, 5).Select(index =>
-      new WeatherForecast
-      (
-          DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-          Random.Shared.Next(-20, 55),
-          summaries[Random.Shared.Next(summaries.Length)]
-      ))
-      .ToArray();
-  return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-  public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
