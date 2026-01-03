ca471e35743cc76fede133ac8ddd931e59272664
joshdfuqua <joshdfuqua@gmail.com>
2026-01-03 10:38:38 -0500

    Remove unused methods from service
    - Removes methods in order to better follow vertical slice approach.  Methods will be reintroduced later

--- src/FamMan.Api.Calendars/Interfaces/ICalendarService.cs
+++ src/FamMan.Api.Calendars/Interfaces/ICalendarService.cs
@@ -5,8 +5,5 @@ namespace FamMan.Api.Calendars.Interfaces;
 
 public interface ICalendarService
 {
-  public Task<CalendarResponseDto> CreateCalendarAsync(CalendarEntity entity, CancellationToken ct);
-  public Task<(string status, CalendarResponseDto? dto)> UpdateCalendarAsync(CalendarEntity entity, CancellationToken ct);
-  public Task<(string status, CalendarResponseDto? dto)> GetCalendarAsync(Guid id, CancellationToken ct);
-  public Task DeleteCalendarAsync(Guid id, CancellationToken ct);
+  public Task<CalendarResponseDto> CreateCalendarAsync(CalendarRequestDto dto, CancellationToken ct);
 }
