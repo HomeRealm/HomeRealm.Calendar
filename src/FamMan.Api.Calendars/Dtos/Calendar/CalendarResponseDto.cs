5ef4f8518e7ef5d84e2c248f56ff7d14e30ae6da
joshdfuqua <joshdfuqua@gmail.com>
2026-01-06 16:34:31 -0500

    Update namespaces for Calendar DTOs

--- src/FamMan.Api.Calendars/Dtos/Calendar/CalendarResponseDto.cs
+++ src/FamMan.Api.Calendars/Dtos/Calendar/CalendarResponseDto.cs
@@ -1,6 +1,6 @@
 using FamMan.Api.Calendars.Interfaces;
 
-namespace FamMan.Api.Calendars.Dtos;
+namespace FamMan.Api.Calendars.Dtos.Calendar;
 
 /// <inheritdoc/>
 public class CalendarResponseDto : ICalenderResponseDto
