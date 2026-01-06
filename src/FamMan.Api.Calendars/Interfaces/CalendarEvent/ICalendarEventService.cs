528fe8c90c00bfba57ea674e1356e79b3fb59538
joshdfuqua <joshdfuqua@gmail.com>
2026-01-06 16:58:26 -0500

    Adds variable names to CalendarEventService interface

--- src/FamMan.Api.Calendars/Interfaces/CalendarEvent/ICalendarEventService.cs
+++ src/FamMan.Api.Calendars/Interfaces/CalendarEvent/ICalendarEventService.cs
@@ -6,8 +6,8 @@ public interface ICalendarEventService
 {
 
   public Task<CalendarEventResponseDto> CreateCalendarEventAsync(CalendarEventRequestDto dto, CancellationToken ct);
-  public Task<(string, CalendarEventResponseDto?)> UpdateCalendarEventAsync(CalendarEventRequestDto dto, Guid id, CancellationToken ct);
-  public Task<(string, CalendarEventResponseDto?)> GetCalendarEventAsync(Guid id, CancellationToken ct);
+  public Task<(string status, CalendarEventResponseDto? updatedCalendarEvent)> UpdateCalendarEventAsync(CalendarEventRequestDto dto, Guid id, CancellationToken ct);
+  public Task<(string status, CalendarEventResponseDto? calendarEvent)> GetCalendarEventAsync(Guid id, CancellationToken ct);
   public Task<List<CalendarEventResponseDto>> GetAllCalendarEventsAsync(CancellationToken ct);
   public Task DeleteCalendarEventAsync(Guid id, CancellationToken ct);
 }
